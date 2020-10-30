7c769503360ef127160399cb9aff9b2d85fc702e
Kody Rogers <kodyrogers21@gmail.com>
2020-10-30 17:46:23 -0600

    Use Approx function to make the tests involving floats better

--- custom_libraries/DHT_U.cpp
+++ custom_libraries/DHT_U.cpp
@@ -1,5 +1,5 @@
 #include "catch.hpp"
-#include "bin_sensor_library/DHT_U.hpp"
+#include "bin_sensor_library/DHT_U.h"
 
 
 TEST_CASE("Test positive values", "[DHT-U]") {
@@ -9,25 +9,25 @@ TEST_CASE("Test positive values", "[DHT-U]") {
     
     sensor.temperature().getEvent(&new_event);
     
-    REQUIRE(new_event.temperature == (float)0.7);
+    REQUIRE(new_event.temperature == Approx((float)0.7));
     
     sensor.temp = 1.8;
     
     sensor.temperature().getEvent(&new_event);
     
-    REQUIRE(new_event.temperature == (float)1.8);
+    REQUIRE(new_event.temperature == Approx((float)1.8));
     
     sensor.temp = 5.4;
     
     sensor.temperature().getEvent(&new_event);
     
-    REQUIRE(new_event.temperature == (float)5.4);
+    REQUIRE(new_event.temperature == Approx((float)5.4));
     
     sensor.temp = 100.5;
     
     sensor.temperature().getEvent(&new_event);
     
-    REQUIRE(new_event.temperature == (float)100.5);
+    REQUIRE(new_event.temperature == Approx((float)100.5));
 }
 
 TEST_CASE("Test negative values", "[DHT-U]") {
@@ -37,25 +37,25 @@ TEST_CASE("Test negative values", "[DHT-U]") {
   
   sensor.temperature().getEvent(&new_event);
   
-  REQUIRE(new_event.temperature == (float)-0.7);
+  REQUIRE(new_event.temperature == Approx((float)-0.7));
   
   sensor.temp = -1.8;
   
   sensor.temperature().getEvent(&new_event);
   
-  REQUIRE(new_event.temperature == (float)-1.8);
+  REQUIRE(new_event.temperature == Approx((float)-1.8));
   
   sensor.temp = -5.4;
   
   sensor.temperature().getEvent(&new_event);
   
-  REQUIRE(new_event.temperature == (float)-5.4);
+  REQUIRE(new_event.temperature == Approx((float)-5.4));
   
   sensor.temp = -100.5;
   
   sensor.temperature().getEvent(&new_event);
   
-  REQUIRE(new_event.temperature == (float)-100.5);
+  REQUIRE(new_event.temperature == Approx((float)-100.5));
 }
 
 TEST_CASE("Test special values", "[DHT-U]") {
@@ -65,13 +65,13 @@ TEST_CASE("Test special values", "[DHT-U]") {
   
   sensor.temperature().getEvent(&new_event);
   
-  REQUIRE(new_event.temperature == (float)0.0);
+  REQUIRE(new_event.temperature == Approx((float)0.0));
   
   sensor.temp = -0.0;
   
   sensor.temperature().getEvent(&new_event);
   
-  REQUIRE(new_event.temperature == (float)-0.0);
+  REQUIRE(new_event.temperature == Approx((float)-0.0));
   
   sensor.temp = 0.0 / 0.0;
   
