ec0d2cb8cb26bf2907dc43883eb4d5873f83c2d3
Kody Rogers <kodyrogers21@gmail.com>
2020-10-30 15:17:47 -0600

    Add tests for the Mock DHT_U library (#18)

---
+++ custom_libraries/DHT_U.cpp
@@ -0,0 +1,83 @@
+#include "catch.hpp"
+#include "bin_sensor_library/DHT_U.hpp"
+
+
+TEST_CASE("Test positive values", "[DHT-U]") {
+    DHT_Unified sensor(0.7);
+    
+    sensors_event_t new_event;
+    
+    sensor.temperature().getEvent(&new_event);
+    
+    REQUIRE(new_event.temperature == (float)0.7);
+    
+    sensor.temp = 1.8;
+    
+    sensor.temperature().getEvent(&new_event);
+    
+    REQUIRE(new_event.temperature == (float)1.8);
+    
+    sensor.temp = 5.4;
+    
+    sensor.temperature().getEvent(&new_event);
+    
+    REQUIRE(new_event.temperature == (float)5.4);
+    
+    sensor.temp = 100.5;
+    
+    sensor.temperature().getEvent(&new_event);
+    
+    REQUIRE(new_event.temperature == (float)100.5);
+}
+
+TEST_CASE("Test negative values", "[DHT-U]") {
+  DHT_Unified sensor(-0.7);
+  
+  sensors_event_t new_event;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(new_event.temperature == (float)-0.7);
+  
+  sensor.temp = -1.8;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(new_event.temperature == (float)-1.8);
+  
+  sensor.temp = -5.4;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(new_event.temperature == (float)-5.4);
+  
+  sensor.temp = -100.5;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(new_event.temperature == (float)-100.5);
+}
+
+TEST_CASE("Test special values", "[DHT-U]") {
+  DHT_Unified sensor(0.0);
+  
+  sensors_event_t new_event;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(new_event.temperature == (float)0.0);
+  
+  sensor.temp = -0.0;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(new_event.temperature == (float)-0.0);
+  
+  sensor.temp = 0.0 / 0.0;
+  
+  sensor.temperature().getEvent(&new_event);
+  
+  REQUIRE(std::isnan(new_event.temperature) == true);
+}
+
+
